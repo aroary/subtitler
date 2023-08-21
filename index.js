ef0067f0a3a02a1388f04d98332bb79ff1d4667d
aroary <me@aroary.com>
2023-08-20 20:16:48 -0400

    proto

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const fluent = require("fluent-ffmpeg");
 const openAI = require("openai");
 const express = require("express");
 
@@ -8,22 +9,42 @@ const openai = new openAI.OpenAI({
     timeout: 30000
 });
 
-openai.audio.transcriptions.create({
-    file: fs.createReadStream("test.mp4"),
-    response_format: "srt",
-    model: 'whisper-1',
-})
-    .then(console.log)
-    .catch(console.error);
+fluent.setFfmpegPath(require('@ffmpeg-installer/ffmpeg').path);
 
 const app = express();
 
-app.post("/api/[a-zA-Z0-9]+/upload", (req, res) => {
-    res.status(200).json({ code: 204 });
+app.get("/api/[a-zA-Z0-9]+/subtitle", (req, res) => {
+    // Save the video data to a file.
+    fs.writeFileSync("./test.mp4", req.body);
+
+    // Generate srt subtitles.
+    openai.audio.transcriptions.create({
+        file: fs.createReadStream("test.mp4"),
+        response_format: "srt",
+        model: 'whisper-1',
+    })
+        .then(transcription => {
+            // Save srt file with transcription.
+            fs.writeFileSync("./test.srt", transcription);
+
+            // Add subtitles from srt to video.
+            fluent("./test.mp4")
+                .videoFilters(`subtitles=${"./test.srt"}`)
+                .output("./tested.mp4")
+                .on('error', error => {
+                    res.status(500).send();
+                    console.error(error);
+                })
+                .on('end', () => {
+                    // Send results
+                    res.status(200).send(fs.readFileSync("./tested.mp4"));
+                })
+                .run();
+        })
+        .catch(error => {
+            res.status(500).send();
+            console.error(error);
+        });
 });
 
-app.get("/api/[a-zA-Z0-9]+/download", (req, res) => {
-    res.status(200).json({ code: 204 });
-});
-
-// app.listen(Number(process.env.OPENAI_API_KEY), () => console.log("http://localhost:3000/"));
+app.listen(Number(process.env.OPENAI_API_KEY), () => console.log(`http://localhost:${process.env.OPENAI_API_KEY}/`));
