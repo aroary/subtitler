eeda9359eb3f602c9c307d4a24c9626604c0d6c8
aroary <me@aroary.com>
2023-08-21 07:43:57 -0400

    working prototype

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const fs = require("fs");
+const path = require('path');
 const fluent = require("fluent-ffmpeg");
 const openAI = require("openai");
+const moment = require("moment");
 const express = require("express");
+const multer = require('multer');
 
+// Add timestamp to console outputs.
+const _log = console.log, _error = console.error;
+console.log = (...args) => _log(moment().format("\\[MM:DD:YY;HH:mm:ss\\]"), "LOG", ...args);
+console.error = (...args) => _error(moment().format("\\[MM:DD:YY;HH:mm:ss\\]"), "ERR", ...args);
+
+// Initialize openAI API
 const openai = new openAI.OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
     maxRetries: 0,
@@ -11,40 +20,72 @@ const openai = new openAI.OpenAI({
 
 fluent.setFfmpegPath(require('@ffmpeg-installer/ffmpeg').path);
 
-const app = express();
-
-app.get("/api/[a-zA-Z0-9]+/subtitle", (req, res) => {
-    // Save the video data to a file.
-    fs.writeFileSync("./test.mp4", req.body);
-
-    // Generate srt subtitles.
-    openai.audio.transcriptions.create({
-        file: fs.createReadStream("test.mp4"),
-        response_format: "srt",
-        model: 'whisper-1',
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: (req, file, cb) => cb(null, './'),
+        filename: (req, file, cb) => cb(null, "test.mp4")
     })
-        .then(transcription => {
-            // Save srt file with transcription.
-            fs.writeFileSync("./test.srt", transcription);
-
-            // Add subtitles from srt to video.
-            fluent("./test.mp4")
-                .videoFilters(`subtitles=${"./test.srt"}`)
-                .output("./tested.mp4")
-                .on('error', error => {
-                    res.status(500).send();
-                    console.error(error);
-                })
-                .on('end', () => {
-                    // Send results
-                    res.status(200).send(fs.readFileSync("./tested.mp4"));
-                })
-                .run();
-        })
-        .catch(error => {
-            res.status(500).send();
-            console.error(error);
-        });
 });
 
-app.listen(Number(process.env.OPENAI_API_KEY), () => console.log(`http://localhost:${process.env.OPENAI_API_KEY}/`));
+const app = express();
+
+app.use('/', express.static(path.join(__dirname, './view')));
+// app.use(express.raw({ type: "video/mp4" }));
+
+app.post("/subtitle", upload.fields([{ name: "video", maxCount: 1 }]), (req, res) => {
+    try {
+        // Save the video data to a file.
+        console.log("Saving uploaded file");
+
+        // Check for errors.
+        fluent("./test.mp4")
+            .addInputOption('-xerror')
+            .addInputOption('-v error')
+            .output('-')
+            .outputOptions('-f null')
+            .on('error', error => {
+                res.status(400).send();
+                console.error(error.message);
+            })
+            .on("end", () => {
+                // Generate srt subtitles.
+                console.log("Generating transcription");
+                openai.audio.transcriptions.create({
+                    file: fs.createReadStream("test.mp4"),
+                    response_format: "srt",
+                    model: 'whisper-1',
+                })
+                    .then(transcription => {
+                        // Save srt file with transcription.
+                        console.log("Saving transcription");
+                        fs.writeFileSync("./test.srt", transcription);
+
+                        // Add subtitles from srt to video.
+                        console.log("Generating file");
+                        fluent("./test.mp4")
+                            .videoFilters(`subtitles=${"./test.srt"}`)
+                            .output("./tested.mp4")
+                            .on('error', error => {
+                                res.status(500).send();
+                                console.error(error.message);
+                            })
+                            .on('end', () => {
+                                // Send results
+                                console.log("Sending generated file");
+                                res.setHeader("Content-Disposition", "attachment").status(200).send(fs.readFileSync("./tested.mp4"));
+                            })
+                            .run();
+                    })
+                    .catch(error => {
+                        res.status(500).send();
+                        console.error(error.message);
+                    });
+            })
+            .run();
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send();
+    }
+});
+
+app.listen(Number(process.env.PORT), () => console.log(`http://localhost:${process.env.PORT}/`));
