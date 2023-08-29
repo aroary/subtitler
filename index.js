b05955e3972c306e1d50a2f8fbe237a2848093f7
aroary <me@aroary.com>
2023-08-29 11:46:41 -0400

    async generating

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use("/", express.static(path.join(__dirname, './public')));
 
 app.get(["/", "/home"], (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
 
-app.post("/subtitle", upload.fields([{ name: "video", maxCount: 1 }]), (req, res) => {
+app.post("/subtitle", upload.fields([{ name: "video", maxCount: 1 }]), async (req, res) => {
     const chunk = {
         log(...args) {
             if (req.accepts("text/html")) res.write(`<code>${args.map(String).join(" ")}</code>`);
@@ -53,102 +53,72 @@ app.post("/subtitle", upload.fields([{ name: "video", maxCount: 1 }]), (req, res
     if (req.accepts("text/html")) {
         res.status(200);
         res.write(fs.readFileSync(path.join(__dirname, "./public/results.html")));
-        res.write(`<header><span class="logo">SubtitleRush</span></header><div class="download-container"><h1>Hurray!</h1><h2>Your video is ready, check it out.</h2>`);
+        res.write(`<header><span class="logo">SubtitleRush</span></header><div class="download-container">`);
     }
 
-    // Download from url if provided
-    if (req.body.videoURL && !fs.existsSync("./test.mp4")) (() => download(req.body.videoURL).then(file => chunk.log("Downloaded", file)).catch(chunk.error))();
-    if (fs.existsSync("./test.mp4")) try {
+    try {
+        // Download from url if provided
+        if (req.body.videoURL && !fs.existsSync("./test.mp4")) {
+            const file = await download(req.body.videoURL);
+            chunk.log("Downloaded", file);
+        }
+
         // Error check
         chunk.log("Checking for errors");
-        errorCheck("./test.mp4").then(() => {
-            // Generate srt subtitles.
-            chunk.log("Generating subtitles");
-            transcribe(openai, "./test.mp4").then(async transcription => {
-                // Save srt file with transcription.
-                chunk.log("Saving subtitles");
-                fs.writeFileSync("./test.srt", transcription);
+        await errorCheck("./test.mp4");
 
-                try {
-                    // Add subtitles from srt to video.
-                    chunk.log("Burning subtitles");
-                    await burn("./test.mp4", "./test.srt", "./tested.mp4");
+        // Generate srt subtitles.
+        chunk.log("Generating subtitles");
+        transcription = await transcribe(openai, "./test.mp4");
 
-                    chunk.log("Updating results");
-                    fs.unlinkSync("./test.mp4");
-                    fs.renameSync("./tested.mp4", "./test.mp4");
+        // Save srt file with transcription.
+        chunk.log("Saving subtitles");
+        fs.writeFileSync("./test.srt", transcription);
 
-                    chunk.log("Embedding subtitles");
-                    await embed("./test.mp4", "./test.srt", "./tested.mp4");
+        // Add subtitles from srt to video.
+        chunk.log("Burning subtitles");
+        await burn("./test.mp4", "./test.srt", "./tested.mp4");
 
-                    // Send results
-                    chunk.log("Sending results");
+        chunk.log("Updating results");
+        fs.unlinkSync("./test.mp4");
+        fs.renameSync("./tested.mp4", "./test.mp4");
 
-                    const results = {
-                        "videoData": fs.readFileSync(path.join('./tested.mp4')).toString('base64'),
-                        "transcript": fs.readFileSync(path.join('./test.srt')).toString('base64'),
-                        "videoFile": "test.mp4",
-                        "transcriptFile": "test.srt"
-                    };
+        chunk.log("Embedding subtitles");
+        await embed("./test.mp4", "./test.srt", "./tested.mp4");
 
-                    if (req.accepts("text/html")) {
-                        res.write(`<video controls><source src="data:video/mp4;base64,${results.videoData}" type="video/mp4"></video>`);
-                        res.write(`<a class="download-btn" download="${results.videoFile}" href="data:video/mp4;base64,${results.videoData}">Download Video</a>`);
-                        res.write(`<a class="download-btn download-transcript-btn" download="${results.transcriptFile}" href="data:application/x-subrip;base64,${results.transcript}">Download Transcription</a>`);
-                    } else {
-                        res.charset = "base46";
-                        res.status(200).type("mp4").send(results.videoData);
-                    }
-                } catch (error) {
-                    chunk.error(error.message);
-                } finally {
-                    // Clean up
-                    chunk.log("Cleaning files");
-                    fs.unlinkSync("./test.mp4");
-                    fs.unlinkSync("./tested.mp4");
-                    fs.unlinkSync("./test.srt");
-                    fs.unlinkSync("./test.ass");
+        // Send results
+        chunk.log("Sending results");
 
-                    if (req.accepts("text/html")) {
-                        res.write(`</div><footer><p>Copyright &copy; 2023 Kuma Web Creations</p><a href="https://wearekuma.com" target="_blank"><img src="./media/kuma-logo.png" alt="Kuma Web Creations"></a></footer>`);
-                        res.end();
-                    };
-                }
-            }).catch(error => {
-                chunk.error(error.message);
+        const results = {
+            videoData: fs.readFileSync(path.join('./tested.mp4')).toString('base64'),
+            transcript: fs.readFileSync(path.join('./test.srt')).toString('base64'),
+            videoFile: "test.mp4",
+            transcriptFile: "test.srt"
+        };
 
-                // Clean up
-                chunk.log("Cleaning files");
-                fs.unlink("./test.mp4", error => (error ? chunk.error : chunk.log)(error || "Deleted test.mp4"));
-
-                if (req.accepts("text/html")) {
-                    res.write(`</div><footer><p>Copyright &copy; 2023 Kuma Web Creations</p><a href="https://wearekuma.com" target="_blank"><img src="./media/kuma-logo.png" alt="Kuma Web Creations"></a></footer>`);
-                    res.end();
-                };
-            });
-        }).catch(error => {
-            chunk.error(error.message);
-
-            // Clean up
-            chunk.log("Cleaning files");
-            fs.unlink("./test.mp4", error => (error ? chunk.error : chunk.log)(error || "Deleted test.mp4"));
-
-            if (req.accepts("text/html")) {
-                res.write(`</div><footer><p>Copyright &copy; 2023 Kuma Web Creations</p><a href="https://wearekuma.com" target="_blank"><img src="./media/kuma-logo.png" alt="Kuma Web Creations"></a></footer>`);
-                res.end();
-            };
-        });
+        if (req.accepts("text/html")) {
+            res.write(`<h1>Hurray!</h1><h2>Your video is ready, check it out.</h2>`);
+            res.write(`<video controls><source src="data:video/mp4;base64,${results.videoData}" type="video/mp4"></video>`);
+            res.write(`<a class="download-btn" download="${results.videoFile}" href="data:video/mp4;base64,${results.videoData}">Download Video</a>`);
+            res.write(`<a class="download-btn download-transcript-btn" download="${results.transcriptFile}" href="data:application/x-subrip;base64,${results.transcript}">Download Transcription</a>`);
+        } else {
+            res.charset = "base46";
+            res.status(200).type("mp4").send(results.videoData);
+        }
     } catch (error) {
-        chunk.error(chunk.message);
-
-        // Clean up
-        chunk.log("Cleaning files");
-        fs.unlink("./test.mp4", error => (error ? chunk.error : chunk.log)(error || "Deleted test.mp4"));
-
+        chunk.error(error.message);
+    } finally {
         if (req.accepts("text/html")) {
             res.write(`</div><footer><p>Copyright &copy; 2023 Kuma Web Creations</p><a href="https://wearekuma.com" target="_blank"><img src="./media/kuma-logo.png" alt="Kuma Web Creations"></a></footer>`);
             res.end();
         };
+
+        // Clean up
+        console.log("Cleaning files");
+        fs.unlink("./test.mp4", error => !error && console.log("Deleted test.mp4"));
+        fs.unlink("./tested.mp4", error => !error && console.log("Deleted tested.mp4"));
+        fs.unlink("./test.srt", error => !error && console.log("Deleted test.srt"));
+        fs.unlink("./test.ass", error => !error && console.log("Deleted test.ass"));
     }
 });
 
