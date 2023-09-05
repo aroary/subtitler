73a3124d8a93a1b016e0d9214aed04c8b2e85e9a
aroary <me@aroary.com>
2023-09-05 18:45:52 -0400

    Resolves #3

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 const path = require('path');
-const openAI = require("openai");
 const moment = require("moment");
 const express = require("express");
 const multer = require('multer');
@@ -11,26 +10,27 @@ const { errorCheck, burn, embed } = require("./utils/video");
 
 // Add timestamp to console outputs.
 const _log = console.log, _error = console.error;
-console.log = (...args) => _log(moment().format("\\[MM:DD:YY;HH:mm:ss\\]"), "LOG", ...args);
-console.error = (...args) => _error(moment().format("\\[MM:DD:YY;HH:mm:ss\\]"), "ERR", ...args);
+console.log = (...args) => _log(moment().format("\\[MM:DD:YY HH:mm:ss\\]"), "LOG", ...args);
+console.error = (...args) => _error(moment().format("\\[MM:DD:YY HH:mm:ss\\]"), "ERR", ...args);
 
-// Initialize openAI API.
-const openai = new openAI.OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-    maxRetries: 0,
-    timeout: 30000
-});
+// Initialize generated data directory
+if (!process.argv.includes("--dry-run")) {
+    console.log("Initializing ./data/");
+    if (fs.existsSync(path.join(__dirname, "./data"))) fs.rmSync(path.join(__dirname, "./data"), { recursive: true, force: true });
+    fs.mkdirSync(path.join(__dirname, "./data"));
+};
 
 // Save the video data to a file.
 const upload = multer({
     storage: process.argv.includes("--dry-run") ? null : multer.diskStorage({
-        destination: (req, file, cb) => cb(null, './'),
+        destination: (req, file, cb) => cb(null, './data'),
         filename: (req, file, cb) => {
-            console.log("Saving uploaded file");
-            return cb(null, "test.mp4");
+            const filename = `${Date.now()}_${file.originalname}`;
+            console.log("Saving", filename);
+            return cb(null, filename);
         }
     })
-});
+}).single("video");
 
 const app = express();
 
@@ -39,7 +39,7 @@ app.use("/", express.static(path.join(__dirname, './public')));
 
 app.get(["/", "/home"], (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
 
-app.post("/subtitle", upload.fields([{ name: "video", maxCount: 1 }]), async (req, res) => {
+app.post("/subtitle", upload, async (req, res) => {
     const chunk = {
         log(...args) {
             if (req.accepts("text/html")) res.write(`<script>status(${JSON.stringify(args.map(String).join(" "))})</script>`);
@@ -57,56 +57,63 @@ app.post("/subtitle", upload.fields([{ name: "video", maxCount: 1 }]), async (re
         res.write(`<script src="./chunk.js"></script>`);
     }
 
+    var file, name, type;
+
     try {
-        // Download from url if provided
-        if (req.body.videoURL && !fs.existsSync("./test.mp4")) {
-            const file = await download(req.body.videoURL);
-            chunk.log("Downloaded", file);
-        }
+        // Get file
+        file = req.file?.filename || await download(req.body.videoURL);
+        name = file.split(".").slice(0, -1).join(".");
+        type = file.split(".").pop();
 
         // Error check
         chunk.log("Checking for errors");
-        await errorCheck("./test.mp4");
+        await errorCheck(`./data/${file}`);
 
         // Generate srt subtitles.
         chunk.log("Generating subtitles");
-        transcription = await transcribe(openai, "./test.mp4");
+        transcription = await transcribe(`./data/${file}`);
 
         // Save srt file with transcription.
         chunk.log("Saving subtitles");
-        if (!process.argv.includes("--dry-run")) fs.writeFileSync("./test.srt", transcription);
+        if (!process.argv.includes("--dry-run")) fs.writeFileSync(`./data/${name}.srt`, transcription);
 
         // Add subtitles from srt to video.
         chunk.log("Burning subtitles");
-        await burn("./test.mp4", "./test.srt", "./tested.mp4");
+        await burn(`./data/${file}`, `./data/${name}.srt`, `./data/_${file}`);
 
         chunk.log("Updating results");
         if (!process.argv.includes("--dry-run")) {
-            fs.unlinkSync("./test.mp4");
-            fs.renameSync("./tested.mp4", "./test.mp4");
+            fs.unlinkSync(`./data/${file}`);
+            fs.renameSync(`./data/_${file}`, `./data/${file}`);
         };
 
         chunk.log("Embedding subtitles");
-        await embed("./test.mp4", "./test.srt", "./tested.mp4");
+        await embed(`./data/${file}`, `./data/${name}.srt`, `./data/_${file}`);
+
+        chunk.log("Updating results");
+        if (!process.argv.includes("--dry-run")) {
+            fs.unlinkSync(`./data/${file}`);
+            fs.renameSync(`./data/_${file}`, `./data/${file}`);
+        };
 
         // Send results
         chunk.log("Sending results");
 
         const results = {
-            videoData: process.argv.includes("--dry-run") ? "" : fs.readFileSync(path.join('./tested.mp4')).toString('base64'),
-            transcript: process.argv.includes("--dry-run") ? "" : fs.readFileSync(path.join('./test.srt')).toString('base64'),
-            videoFile: process.argv.includes("--dry-run") ? "" : "test.mp4",
-            transcriptFile: process.argv.includes("--dry-run") ? "" : "test.srt"
+            videoData: process.argv.includes("--dry-run") ? "" : fs.readFileSync(path.join(`./data/${file}`)).toString('base64'),
+            transcript: process.argv.includes("--dry-run") ? "" : fs.readFileSync(path.join(`./data/${name}.srt`)).toString('base64'),
+            videoFile: process.argv.includes("--dry-run") ? "" : file.replace(/\d+_/, ""),
+            transcriptFile: process.argv.includes("--dry-run") ? "" : name.replace(/\d+_/, "") + ".srt"
         };
 
         if (req.accepts("text/html")) {
             res.write(`<h1>Hurray!</h1><h2>Your video is ready, check it out.</h2>`);
-            res.write(`<video controls><source src="data:video/mp4;base64,${results.videoData}" type="video/mp4"></video>`);
+            res.write(`<video controls><source src="data:video/${type};base64,${results.videoData}" type="video/${type}"></video>`);
             res.write(`<a class="download-btn" download="${results.videoFile}" href="data:video/mp4;base64,${results.videoData}">Download Video</a>`);
             res.write(`<a class="download-btn download-transcript-btn" download="${results.transcriptFile}" href="data:application/x-subrip;base64,${results.transcript}">Download Transcription</a>`);
         } else {
             res.charset = "base46";
-            res.status(200).type("mp4").send(results.videoData);
+            res.status(200).type(type).send(results.videoData);
         }
     } catch (error) {
         chunk.error(error.message);
@@ -122,10 +129,9 @@ app.post("/subtitle", upload.fields([{ name: "video", maxCount: 1 }]), async (re
         // Clean up
         console.log("Cleaning files");
         if (!process.argv.includes("--dry-run")) {
-            fs.unlink("./test.mp4", error => !error && console.log("Deleted test.mp4"));
-            fs.unlink("./tested.mp4", error => !error && console.log("Deleted tested.mp4"));
-            fs.unlink("./test.srt", error => !error && console.log("Deleted test.srt"));
-            fs.unlink("./test.ass", error => !error && console.log("Deleted test.ass"));
+            fs.unlink(`./data/${file}`, error => !error && console.log(`Deleted ${file}`));
+            fs.unlink(`./data/${name}.srt`, error => !error && console.log(`Deleted ${name}.srt`));
+            fs.unlink(`./data/${name}.ass`, error => !error && console.log(`Deleted ${name}.ass`));
         }
     }
 });
--- utils/transcribe.js
+++ utils/transcribe.js
@@ -1,14 +1,20 @@
 const fs = require("fs");
 const openAI = require("openai");
 
+// Initialize openAI API.
+const openai = new openAI.OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+    maxRetries: 0,
+    timeout: 30000
+});
+
 /**
  * @description Get transcription of video.
- * @param {openAI.OpenAI} openai - configurated openAI
  * @param {string} video - The video
  * @param {"srt"|"text"} format - Format of transcription file
  * @returns {Promise} Video transcription.
  */
-function transcibe(openai, video, format = "srt") {
+function transcibe(video, format = "srt") {
     if (process.argv.includes("--dry-run")) return new Promise(r => r(""));
     else return openai.audio.transcriptions.create({
         file: fs.createReadStream(video),
